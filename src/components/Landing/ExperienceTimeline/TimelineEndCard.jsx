8426264a95ae3a168295ac5fdb4ff5f1c483e46f
Henry Pineda <pinedahenryjre@gmail.com>
2026-05-04 16:23:42 +0800

    chore: added a new resume and renamed in frontend

--- src/components/Landing/ExperienceTimeline/TimelineEndCard.jsx
+++ src/components/Landing/ExperienceTimeline/TimelineEndCard.jsx
@@ -65,7 +65,7 @@ const TimelineEndCard = () => {
               <p className="text-sm font-medium text-foreground">or</p>
               <Button size="lg" className="px-8 w-[30vw] md:w-auto" variant="secondary" asChild>
                 <a
-                  href="/my-resume.pdf"
+                  href="/resume-henry-pineda.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="View My Resume (opens in new tab)"
